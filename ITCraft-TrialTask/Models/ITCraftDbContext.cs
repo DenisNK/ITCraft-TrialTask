de0cfb260da3a4ca1e6d9e5e174a0ba9e0ffd82c
Denis Yrchenko <denis3101996@gmail.com>
2021-02-18 16:59:35 +0300

    Add project files.

---
+++ ITCraft-TrialTask/Models/ITCraftDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITCraft_TrialTask.Models
+{
+    public class ITCraftDbContext : DbContext
+    {                                              
+
+        public ITCraftDbContext(DbContextOptions<ITCraftDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users{ get; set; }
+    }
+}
