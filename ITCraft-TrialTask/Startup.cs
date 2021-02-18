ce8b9f1b241f0514b3f2b56fa707880b03c853a8
Denis Yrchenko <denis3101996@gmail.com>
2021-02-18 19:11:54 +0300

    Init

--- ITCraft-TrialTask/Startup.cs
+++ ITCraft-TrialTask/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ITCraft_TrialTask.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace ITCraft_TrialTask
 {
@@ -28,7 +23,10 @@ namespace ITCraft_TrialTask
         {
 
             services.AddControllers();
-
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentAPI", Version = "v1" });
+            });
             services.AddDbContext<ItCraftDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
@@ -39,6 +37,9 @@ namespace ITCraft_TrialTask
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentAPI v1"));
+
             }
 
             app.UseRouting();
